60c0d52676bad2014a7f7b74105ea3c678357172
Prof Lab05 <prof@etec.com>
2025-11-27 16:39:33 -0300

    atendente listagem

--- src/Telas/Atendentes/Atendentes.js
+++ src/Telas/Atendentes/Atendentes.js
@@ -1,10 +1,35 @@
 import CardPesquisa from '../../Componente/card-pesquisa/card_pesquisa'
-
+import {useState, useEffect} from 'react'
 export default function Atendentes(){
+    const [lista, setLista] = useState([])
+
+    useEffect( async () =>{
+        const resp = await fetch('http://localhost:8081/sw1b/rotas/atendente')
+        const dados = resp.json()
+        setLista(dados)
+    })
+
+
     return(
         <div>
             <h1>Atendentes</h1>
             <CardPesquisa nome="Atendente"></CardPesquisa>
+            <table className='table table-striped table-houver'>
+                <thead>
+                    <tr>
+                        {lista.map((chave, valor) => {
+                            <th>{chave} - {valor}</th>
+                        })}
+                    </tr>
+                </thead>
+                <tbody>
+                    {lista.map((chave, valor) => {
+                        <tr>
+                            <td>{chave} - {valor}</td>
+                            </tr>
+                        })}
+                </tbody>
+            </table>
         </div>
     )
 }
